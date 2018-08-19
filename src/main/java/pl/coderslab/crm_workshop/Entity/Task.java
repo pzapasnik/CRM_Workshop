c8a60d33aae13b58b7f9d9b1c66a3b438718a97f
pzapasnik <pzapasnik@gmail.com>
2018-08-19 12:50:08 +0200

    First sketch of Task Entity

--- src/main/java/pl/coderslab/crm_workshop/Entity/Task.java
+++ src/main/java/pl/coderslab/crm_workshop/Entity/Task.java
@@ -1,4 +1,109 @@
 package pl.coderslab.crm_workshop.Entity;
 
+import org.hibernate.annotations.CreationTimestamp;
+import pl.coderslab.crm_workshop.Enum.TaskPriority;
+
+import javax.persistence.*;
+import javax.xml.crypto.Data;
+import java.util.List;
+
+@Entity
+@Table(name = "tasks")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private Data created;
+
+    private String subject;
+
+    private Project project;
+
+    private String description;
+
+    private TaskStatus status;
+
+    private TaskPriority priority;
+
+    private User menager;
+
+    private List<User> workers;
+
+    public Task() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Data getCreated() {
+        return created;
+    }
+
+    public void setCreated(Data created) {
+        this.created = created;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public TaskPriority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(TaskPriority priority) {
+        this.priority = priority;
+    }
+
+    public User getMenager() {
+        return menager;
+    }
+
+    public void setMenager(User menager) {
+        this.menager = menager;
+    }
+
+    public List<User> getWorkers() {
+        return workers;
+    }
+
+    public void setWorkers(List<User> workers) {
+        this.workers = workers;
+    }
 }
